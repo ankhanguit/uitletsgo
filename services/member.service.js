77ab6f47064589aa139f85706826fa08e13dd688
An Khang <anhkhanguit@gmail.com>
2016-10-27 22:34:35 +0700

    sfasfasd

--- services/member.service.js
+++ services/member.service.js
@@ -357,11 +357,10 @@ function getMember(groupId, memberId){
 
                 // get all members
                 groupDb.aggregate([
-                        { $match: {'MEMBER_ID': mongo.helper.toObjectID(memberId)}},
-                        { $lookup: { from: "users", localField: "MEMBER_ID", foreignField: "_id", as: "MEMBER_INFO"}},
-                        { $unwind : "$MEMBER_INFO"},
-                        { $project: { MEMBER_ID : 1, JOIN_DATE: 1, ROLE: 1, FIRSTNAME : "$MEMBER_INFO.FIRSTNAME" ,LASTNAME : "$MEMBER_INFO.LASTNAME"}}
-                    ],
+                        { $lookup: { from: "users", localField: "MEMBER_ID", foreignField: "_id", as: "AUTHOR_INFO"}},
+                        { $unwind : "$AUTHOR_INFO"},
+                        { $project: { MEMBER_ID : 1, GROUP_ID : 1, FIRSTNAME : "$AUTHOR_INFO.FIRSTNAME" ,LASTNAME : "$AUTHOR_INFO.LASTNAME"}}
+                ],
                     function (err, members) {
                         if (err){
                             // database error
