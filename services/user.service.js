762ae3f167f21d7ead5bf8c85789c6eb6b1e59d3
An Khang <anhkhanguit@gmail.com>
2016-11-05 14:05:16 +0700

    fasdf

--- services/user.service.js
+++ services/user.service.js
@@ -423,11 +423,7 @@ function search(text) {
     var deferred = Q.defer();
     // prepare query
     db.users.aggregate([
-        { $match: {$or: [{ USERNAME:  new RegExp(text, "i")}, { FIRSTNAME: new RegExp(text, "i") },  { LASTNAME: new RegExp(text, "i") }]}},
-        { $lookup: { from: "avatars", localField: "_id", foreignField: "USER_ID", as: "AVATAR_INFO"}},
-        { $limit : 10 },
-        { $unwind : "$AVATAR_INFO"},
-        { $project: { _id: 1, USERNAME : 1, FIRSTNAME : 1, LASTNAME : 1, EMAIL: 1, PHONE: 1, AVATAR : "$AVATAR_INFO.AVATAR"}}
+            { $match: {$or: [{ USERNAME:  new RegExp(text, "i")}, { FIRSTNAME: new RegExp(text, "i") },  { LASTNAME: new RegExp(text, "i") }]}}
 
     ],function (err, users) {
         if (err){
