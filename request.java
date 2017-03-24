065b74a61aa1585c1ae2cf1f74d9ff007b5aee6b
ankhanguit <anhkhanguit@gmail.com>
2017-03-24 11:55:57 +0700

    Create request.java

---
+++ request.java
@@ -0,0 +1,17 @@
+/**
+ * CREATED BY : KHANG-VA
+ * Date : 2017/03/17
+ * Copyright (c) 2000-2008 FUJINET, All Rights Reserved.
+ * 
+ */
+
+package vn.fjs.live.dto.request;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode
+public class Request {
+
+}
